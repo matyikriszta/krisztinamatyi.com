34f09642512320f87723426bab24f9d0eb6dce6b
matyikriszta <matyi.kriszta@gmail.com>
2020-08-26 22:56:02 -0700

    Reworks ProjectCard component layout

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ProjectCard = styled.div`
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
+const ProjectContent = styled.div`
   margin-bottom: 1.45rem;
+  @media (min-width: 600px) {
+    flex-basis: calc(${props => props.width});
+  }
+`
+const ProjectImage = styled.div`
+  box-shadow: 0 2px 20px rgba(0, 0, 0, 0.08);
   @media (min-width: 600px) {
     flex-basis: calc(${props => props.width} - 1.45rem);
   }
 `
+
 const Image = styled.img`
-  box-shadow: 0 2px 20px rgba(0, 0, 0, 0.08);
+  width: 100%;
 `
 
 export default ({ children, image, alt, title, link, width = '50%' }) => (
-  <ProjectCard width={width}>
-    <Image src={image} alt={alt} />
-    <p>
-      <strong>{title}</strong> - {children}
-    </p>
-    <a href={link}>Visit site</a>
-  </ProjectCard>
+  <FlexContainer>
+    <ProjectContent width={width}>
+      <p>
+        <strong>
+          <a href={link}>{title}</a>
+        </strong>{' '}
+        - {children}
+      </p>
+    </ProjectContent>
+    <ProjectImage width={width}>
+      <Image src={image} alt={alt} />
+    </ProjectImage>
+  </FlexContainer>
 )
